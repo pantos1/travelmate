b4a8ec39a30427ac33cce9f973b7325e2f408024
pantos1 <piotrek.antosiuk@gmail.com>
2019-03-31 18:55:04 +0200

    Initial commit of new setup

--- App.js
+++ App.js
@@ -1,34 +1,59 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Plan from './app/views/Plan';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <Plan
-                planTitle={"Borobudur"}
-                organizer={"Jan Kowalski"}
-                startTime={"9:00"}
-                duration={"7h"}
-                sights={mockPlan}
-            />
-        );
-    }
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <Plan
+            planTitle={"Borobudur"}
+            organizer={"Jan Kowalski"}
+            startTime={"9:00"}
+            duration={"7h"}
+            sights={mockPlan}
+        />
+    );
+  }
 }
 
 const mockPlan = [
-    {
-        key: "Borobudur",
-        price: "200k IND",
-        duration: "2h",
-    }
+  {
+    key: "Borobudur",
+    price: "200k IND",
+    duration: "2h",
+  }
 ];
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
