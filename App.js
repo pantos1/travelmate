70adfc16c3191c7becabaa177ed962c468c20583
pantos1 <piotrek.antosiuk@gmail.com>
2019-05-09 15:14:10 +0200

    SignUp screen with email and password. Form validation. Auth check at the beginning of app working.

--- App.js
+++ App.js
@@ -1,44 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PlanListScreen from "./src/screens/PlanListScreen";
 import { createAppContainer, createStackNavigator, createSwitchNavigator } from "react-navigation";
 import PlanScreen from "./src/screens/PlanScreen";
 import PlanFormScreen from "./src/screens/PlanFormScreen";
 import MapScreen from "./src/screens/MapScreen";
-import { Root } from "native-base";
+import { Root, Spinner } from "native-base";
 import SignUpScreen from "./src/screens/SignUpScreen";
+import firebase from "react-native-firebase";
+import { GoogleSignin } from "react-native-google-signin";
 
-// export default class App extends Component {
-//
-//     constructor(props) {
-//         super(props);
-//         // this.ref = firebase.firestore().collection('trips');
-//         this.state = {
-//             trips: []
-//         };
-//     }
-//
-//     _addKeys = trips => {
-//         return trips.map(trip => {
-//             return Object.assign(trip, { key: trip.id });
-//         });
-//     };
-//
-//     // componentDidMount() {
-//     //     this.ref.get()
-//     //         .then(trips => {
-//     //             this.setState({trips: this._addKeys(trips.docs)});
-//     //         })
-//     //         .catch(error => {
-//     //             console.log('Fetch failed: ', error);
-//     //         });
-//     // };
-//
-//     render() {
-//         return (
-//             <PlanListScreen trips={mockPlans} />
-//         )
-//     }
-// }
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.unsubscriber = null;
+        this.state = {
+            checkedSignIn: false,
+            signedIn: false,
+            user: null
+        };
+    }
+
+    componentDidMount() {
+        this.unsubscriber = firebase.auth().onAuthStateChanged((user) => {
+            this.setState({ user, signedIn: true });
+        });
+        GoogleSignin.getCurrentUser().then((user) => {
+            if(user){
+                this.setState({ user, signedIn: true });
+            }
+        });
+        this.setState({checkedSignIn: true});
+    };
+
+    componentWillUnmount() {
+        if (this.unsubscriber) {
+            this.unsubscriber();
+        }
+    }
+
+    render() {
+        if(!this.state.checkedSignIn) {
+            return <Spinner />
+        }
+
+        const AppNavigator = createAppContainer(switchNavigator(this.state.signedIn));
+
+        return (
+            <Root>
+                <AppNavigator/>
+            </Root>
+        )
+    }
+}
 
 const SignedIn = createStackNavigator(
     {
@@ -60,7 +74,7 @@ const SignedOut = createStackNavigator(
     }
 );
 
-const AppNavigator = (signedIn = false) => createAppContainer(createSwitchNavigator(
+const switchNavigator = (signedIn = false) => createSwitchNavigator(
     {
         SignedIn: {
             screen: SignedIn
@@ -72,10 +86,4 @@ const AppNavigator = (signedIn = false) => createAppContainer(createSwitchNaviga
     {
         initialRouteName: signedIn ? "SignedIn" : "SignedOut"
     }
-));
-
-export default () => (
-    <Root>
-        <AppNavigator/>
-    </Root>
 );
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,37 +1,52 @@
 import React, { Component } from "react";
 import { Button, Container, Form, Input, Item, Text, Toast } from "native-base";
-import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin";
+import { GoogleSignin, GoogleSigninButton, statusCodes } from "react-native-google-signin";
 import firebase from "react-native-firebase";
 import { Formik } from "formik";
+import * as Yup from "yup";
+import { StyleSheet } from "react-native";
 
-const SignUpValidationSchema = {
-
-};
+const SignUpValidationSchema = Yup.object().shape({
+    email: Yup.string().email("Wrong email format").required("Email is required"),
+    password: Yup.string().min(6, "Password has to have at least 6 characters").required("Password is required"),
+    passwordConfirmed: Yup.string().oneOf(
+        [Yup.ref('password')],
+        'Passwords do not match',
+    )
+});
 
 class SignUpScreen extends Component {
 
-    async googleSignIn() {
+    googleSignIn = async() => {
         try {
             await GoogleSignin.configure();
+
+            await GoogleSignin.hasPlayServices();
             
             const data = await GoogleSignin.signIn();
             
-            const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+            const credential = firebase.auth().GoogleAuthProvider.credential(data.idToken, data.accessToken);
 
-            const firebaseUserCredential = await firebase.auth.signInWithCredential(credential);
+            const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
 
         } catch (e) {
+            let text;
+            if (e.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                    text = "Play Services are not available"
+            } else {
+                    text = 'Error while signing in with Google'
+            }
             Toast.show({
-                text: 'Error while signing in with Google',
+                text: text,
                 type: "danger",
                 buttonText: "Dismiss"
             })
         }
-    }
+    };
 
     async signInWithEmail({email, password}) {
         try{
-           const credential = await firebase.auth.createUserWithEmailAndPassword(email, password);
+           const credential = await firebase.auth().createUserWithEmailAndPassword(email, password);
         } catch (e) {
             // TODO: Switch based on error codes
             Toast.show({
@@ -63,18 +78,25 @@ class SignUpScreen extends Component {
                                 <Input
                                     onChangeText={handleChange('email')}
                                     value={values.email}
+                                    keyboardType="email-address"
                                     autoCapitalize="none"
                                     placeholder="Email"
                                 />
+                                {errors.email &&
+                                <Text style={styles.errorText}>{errors.email}</Text>
+                                }
                             </Item>
                             <Item>
                                 <Input
                                     onChangeText={handleChange('password')}
-                                    value={values.email}
+                                    value={values.password}
                                     placeholder="Password"
                                     autoCapitalize="none"
                                     secureTextEntry
                                 />
+                                {errors.password &&
+                                <Text style={styles.errorText}>{errors.password}</Text>
+                                }
                             </Item>
                             <Item>
                                 <Input
@@ -84,6 +106,9 @@ class SignUpScreen extends Component {
                                     autoCapitalize="none"
                                     secureTextEntry
                                 />
+                                {errors.passwordConfirmed &&
+                                <Text style={styles.errorText}>{errors.passwordConfirmed}</Text>
+                                }
                             </Item>
                             <Button onPress={handleSubmit} full>
                                 <Text>
@@ -95,11 +120,19 @@ class SignUpScreen extends Component {
                 </Formik>
                 <GoogleSigninButton
                     onPress={this.googleSignIn}
+                    color={GoogleSigninButton.Color.Auto}
                     size={GoogleSigninButton.Size.Wide}
                     />
             </Container>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    errorText: {
+        fontSize: 10,
+        color: 'red'
+    }
+});
 
 export default SignUpScreen;
